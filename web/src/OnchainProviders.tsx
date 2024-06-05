f6325cc25e9c5efb71a8a4b8e6f5f0c2d94ef04b
starit <starit1992@gmail.com>
2024-06-05 13:44:58 -0700

    fix rpc url for authkit

--- web/src/OnchainProviders.tsx
+++ web/src/OnchainProviders.tsx
@@ -27,7 +27,7 @@ const wagmiConfig = createWagmiConfig(projectId);
  * Todo:: modify the config before the final launch
  */
 const authKitConfig = {
-  rpcUrl: 'https://base.llamarpc.com',
+  rpcUrl: 'https://mainnet.optimism.io',
   domain: 'CastNotes.xyz',
   siweUri: process.env.NEXT_PUBLIC_WEBSITE_URL as string + '/login',
 };
